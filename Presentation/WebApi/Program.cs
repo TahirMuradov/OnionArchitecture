3a6547e3d18e33c56c20bb72ddb62579ee6fc485
TahirMuradov <muradovtahir01@gmail.com>
2025-03-23 13:06:01 +0400

    CorsPlocy Added

--- Presentation/WebApi/Program.cs
+++ Presentation/WebApi/Program.cs
@@ -6,8 +6,16 @@ var builder = WebApplication.CreateBuilder(args);
 // Add services to the container.
 
 builder.Services.AddControllers();
-builder.Services.AddPersistenceServices(); // This is the extension method that registers the services
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
+builder.Services.AddPersistenceServices(); // This is the extension method that registers the services
+builder.Services.AddCors(options =>options.AddDefaultPolicy(
+    policy => policy
+  .WithOrigins(builder.Configuration["Domain:Front"])
+    .AllowAnyMethod()
+    .AllowAnyHeader()
+    )
+
+);
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
 
@@ -19,7 +27,7 @@ if (app.Environment.IsDevelopment())
     app.UseSwagger();
     app.UseSwaggerUI();
 }
-
+app.UseCors();
 app.UseHttpsRedirection();
 
 app.UseAuthorization();
